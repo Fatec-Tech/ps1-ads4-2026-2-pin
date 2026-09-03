6ea31b58ff5c7123e8b346a90905499d194bb842
Pin <mentedudu2006@gmail.com>
2026-09-02 22:21:34 -0300

    adicionado contador, calcula idade e exibição numero de telefone: Murilo

--- aula01-cadastro-pacientes/js/app.js
+++ aula01-cadastro-pacientes/js/app.js
@@ -7,8 +7,26 @@ const tabela = document.getElementById('tabela-pacientes');
 
 // Função responsável por adicionar um paciente ao array
 function adicionarPaciente(nome, email, nascimento, idade, telefone) {
-	const novoPaciente = { nome, email, nascimento, idade, telefone };
+	const novoPaciente = { nome, email, nascimento, idade, telefone, idade: calcularIdade(nascimento) };
 	pacientes.push(novoPaciente);
+}
+
+function calcularIdade(nascimento) {
+	const dataNascimento = new Date(nascimento);
+	const dataAtual = new Date();
+
+	let idade = dataAtual.getFullYear() - dataNascimento.getFullYear();
+	const mes = dataAtual.getMonth() - dataNascimento.getMonth();
+
+	if (mes < 0 || (mes === 0 && dataAtual.getDate() < dataNascimento.getDate())) {
+		idade--;
+	}
+
+	return idade;
+}
+
+function emailRepetido(email) {
+	return pacientes.some((paciente) => paciente.email === email);
 }
 
 // Função responsável por desenhar a tabela inteira a partir do array
@@ -17,13 +35,17 @@ function renderizarTabela() {
 
 	pacientes.forEach((paciente) => {
 		const linha = document.createElement('tr');
+		const contador = document.getElementById('contador-pacientes');
+		contador.textContent = pacientes.length;
+		
+
 
 		linha.innerHTML = `
       <td>${paciente.nome}</td>
       <td>${paciente.email}</td>
       <td>${formatarData(paciente.nascimento)}</td>
-      <td>${Number(paciente.idade)}</td>
-      <td>${Number(paciente.telefone)}</td>
+	  <td>${paciente.telefone}</td>
+		<td>${paciente.idade} anos</td>
     `;
 
 		tabela.appendChild(linha);
@@ -39,12 +61,19 @@ function formatarData(dataISO) {
 // Evento disparado quando o formulário é enviado
 formulario.addEventListener('submit', (event) => {
 	event.preventDefault(); // evita o recarregamento da página
+	
+	const idade = calcularIdade(document.getElementById('nascimento').value);
+
+	if (emailRepetido(email)) {
+	alert('Este email já está cadastrado!');
+	return;
+}
 
 	const nome = document.getElementById('nome').value;
 	const email = document.getElementById('email').value;
 	const nascimento = document.getElementById('nascimento').value;
-
-	adicionarPaciente(nome, email, nascimento);
+	const telefone = document.getElementById('Telefone').value;
+	adicionarPaciente(nome, email, nascimento, idade, telefone);
 	renderizarTabela();
 
 	formulario.reset(); // limpa os campos do formulário
